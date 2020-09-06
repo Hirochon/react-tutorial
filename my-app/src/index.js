5b3c8ad47fd173593b7b74d48446afedf3e29a73
Hirochon <heacet43@gmail.com>
2020-09-06 14:31:35 +0900

    [add] 関数コンポーネント&Hooks化を追加

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
@@ -91,84 +91,85 @@ const Board = (props) => {
   );
 }
 
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
-      stepNumber: 0,
-      xIsNext: true,
-    };
-  }
+// クラスコンポーネント
+// class Game extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       history: [{
+//         squares: Array(9).fill(null),
+//       }],
+//       stepNumber: 0,
+//       xIsNext: true,
+//     };
+//   }
 
-  handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();   // slice()でコピーを作成 pythonで言うlist[:]みたいなもん。
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X': 'O';
-    this.setState({
-      history: history.concat([{
-        squares: squares,
-      }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    })
-  }
+//   handleClick(i) {
+//     const history = this.state.history.slice(0, this.state.stepNumber + 1);
+//     const current = history[history.length - 1];
+//     const squares = current.squares.slice();   // slice()でコピーを作成 pythonで言うlist[:]みたいなもん。
+//     if (calculateWinner(squares) || squares[i]) {
+//       return;
+//     }
+//     squares[i] = this.state.xIsNext ? 'X': 'O';
+//     this.setState({
+//       history: history.concat([{
+//         squares: squares,
+//       }]),
+//       stepNumber: history.length,
+//       xIsNext: !this.state.xIsNext,
+//     })
+//   }
 
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0,
-    });
-  }
+//   jumpTo(step) {
+//     this.setState({
+//       stepNumber: step,
+//       xIsNext: (step % 2) === 0,
+//     });
+//   }
 
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+//   render() {
+//     const history = this.state.history;
+//     const current = history[this.state.stepNumber];
+//     const winner = calculateWinner(current.squares);
 
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>
-            {desc}
-          </button>
-        </li>
-      )
-    })
-    let status;
-    if (winner){
-      status = 'Winner' + winner;
-    } else{
-      status = 'Next Player' + (this.state.xIsNext ? 'X': 'O');
-    }
+//     const moves = history.map((step, move) => {
+//       const desc = move ?
+//         'Go to move #' + move :
+//         'Go to game start';
+//       return (
+//         <li key={move}>
+//           <button onClick={() => this.jumpTo(move)}>
+//             {desc}
+//           </button>
+//         </li>
+//       )
+//     })
+//     let status;
+//     if (winner){
+//       status = 'Winner' + winner;
+//     } else{
+//       status = 'Next Player' + (this.state.xIsNext ? 'X': 'O');
+//     }
 
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board 
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
-    );
-  }
-}
+//     return (
+//       <div className="game">
+//         <div className="game-board">
+//           <Board 
+//             squares={current.squares}
+//             onClick={(i) => this.handleClick(i)}
+//           />
+//         </div>
+//         <div className="game-info">
+//           <div>{status}</div>
+//           <ol>{moves}</ol>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
-function calculateWinner(squares) {
+const calculateWinner = (squares) => {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -186,6 +187,72 @@ function calculateWinner(squares) {
     }
   }
   return null;
+}
+
+// Hooksを使ってく！
+const Game = () => {
+  const [history, setHistory] = useState([{
+    squares: Array(9).fill(null),
+  }]);
+  const [stepNumber, setstepNumber] = useState(0);
+  const [xIsNext, setxIsNext] = useState(true);
+
+  const handleClick = (i) => {
+    const historyUntilCurrentStep = history.slice(0, stepNumber + 1);
+    const current = historyUntilCurrentStep[historyUntilCurrentStep.length - 1];
+    const squares = current.squares.slice();   // slice()でコピーを作成 pythonで言うlist[:]みたいなもん。
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? 'X': 'O';
+    setHistory(historyUntilCurrentStep.concat([{
+      squares: squares,
+    }]));
+    setstepNumber(historyUntilCurrentStep.length);
+    setxIsNext(!xIsNext);
+  }
+
+  const jumpTo = (step) => {
+    setstepNumber(step);
+    setxIsNext((step % 2) === 0);
+  };
+
+  const winner = calculateWinner(history[stepNumber].squares);
+
+  const moves = history.map((step, move) => {
+    const desc = move ?
+      'Go to move #' + move :
+      'Go to game start';
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>
+          {desc}
+        </button>
+      </li>
+    )
+  })
+  let status;
+  if (winner){
+    status = 'Winner' + winner;
+  } else{
+    status = 'Next Player' + (xIsNext ? 'X': 'O');
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board 
+          squares={history[stepNumber].squares}
+          onClick={(i) => handleClick(i)}
+        />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+
 }
 
 // ========================================
