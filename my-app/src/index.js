7f5e9c83bde34bf596a983e9c8c5e887591201bc
Hirochon <heacet43@gmail.com>
2020-09-06 13:34:09 +0900

    [add] boardとsquareで関数コンポーネントバージョンを追加

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
+// クラスコンポーネント
 // class Square extends React.Component {
 //   render() {
 //     return (
@@ -16,6 +16,7 @@ import './index.css';
 //   }
 // }
 
+// 関数コンポーネント
 function Square(props){
   return (
     <button className="square" onClick={props.onClick}>
@@ -24,37 +25,70 @@ function Square(props){
   )
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
+// クラスコンポーネント
+// class Board extends React.Component {
+//   renderSquare(i) {
+//     return (
+//       <Square 
+//         value={this.props.squares[i]}
+//         onClick={() => this.props.onClick(i)}   // onClickでvalueで表示を変える。
+//       />
+//     );
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <div className="board-row">
+//           {this.renderSquare(0)}
+//           {this.renderSquare(1)}
+//           {this.renderSquare(2)}
+//         </div>
+//         <div className="board-row">
+//           {this.renderSquare(3)}
+//           {this.renderSquare(4)}
+//           {this.renderSquare(5)}
+//         </div>
+//         <div className="board-row">
+//           {this.renderSquare(6)}
+//           {this.renderSquare(7)}
+//           {this.renderSquare(8)}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// 関数コンポーネント
+const Board = (props) => {
+  const renderSquare = (i) => {
     return (
       <Square 
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}   // onClickでvalueで表示を変える。
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)}   // onClickでvalueで表示を変える。
       />
     );
   }
 
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 }
 
 class Game extends React.Component {
